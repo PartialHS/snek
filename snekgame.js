843f52e19eb285d2f02244be9c6033a3762b0683
partial <dan.dj@yahoo.com>
2019-11-04 10:38:02 -0500

    fuction
    cause

---
+++ snekgame.js
@@ -0,0 +1,10 @@
+/* makeMap(xVal, yVal)
+makes an arry of array in the format of y number of arrays x long. each one should
+contain a string with just a space in it.
+@param xVal {int} the width of the map
+@param yVal {int} the height of the map
+@return a 2d array of y length of x length items all with spaces inside
+*/
+function makeMap(xVal, yVal){
+  
+}

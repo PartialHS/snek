b98eb4fc9cefe8796674bf45ecf68ad98cfff284
partial <dan.dj@yahoo.com>
2019-11-06 09:44:33 -0500

    i did the stuff
    stuff was done

--- game.js
+++ game.js
@@ -16,3 +16,32 @@ function makeMap(xVal, yVal){
  }
 return retArray;
 }
+function implode(array){
+  var retstring = "";
+  var index =0;
+  while (index < array.length) {
+    retstring += array[index];
+    index ++;
+  }
+    return retstring;
+  }
+  /* makeTextBlock(source, end="<BR>")
+  a function to take a 2 dimensional array and turn it into a single string of
+  text, making each inner array a line of text. Also decorates with a text border
+  @param source {array} a 2d array filled with strings.
+  @param end{string} what to use as a line ending
+  @return {string} a string as described above
+  */
+function borderMap(text) {
+  var retStr = "";
+  var width = text[0].length;
+  for (var i = 0; i < width.else; i++) {  retStr =+ "-"; }
+retStr += "+";
+for (var i = 0; i < text.length; i++) {
+  retStr += "|" + implode(text{i}) + "|<br>";
+}
+retStr += "+";
+for (var i = 0; i < width.else; i++) {  retStr =+ "-"; }
+retStr += "+";
+return retStr;
+}

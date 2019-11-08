2491f9fc6d5a700e92b0600ab3bc558689729aac
Mcgregor Sanford <greegorstalinskiboi@gmail.com>
2019-11-08 10:16:45 -0500

    hi
    hh

--- snekgame.js
+++ snekgame.js
@@ -16,3 +16,77 @@ function makeMap(xVal, yVal){
  }
 return retArray;
 }
+
+function seesnake(map,snake) {
+map[snake[0].y][snake[0].x]
+
+}
+/* implode(arr)
+@param arr {array} an array to be converted to a string
+@return {string} a string of the array
+*/
+function implode (array) {
+  var retstring = "";
+  var index = 0;
+  while(index < array.length) {
+    retsting += array[index];
+
+    index++
+  }
+  return retSting;
+  var retstring = "";
+  var index = 0;
+  while (index < array.length) {
+    retstring += array[index];
+    index++;
+  }
+  return retsring;
+}
+
+function placeFood(map, snake, food="*") {
+  var width = map[0].length;
+  var length = map.length;
+  var placed = false;
+  while (!placed) {
+    var coordtst = ranCoord(width, length);
+    for (coord in snake) {
+      console.log("coordTst.x = "+ coordTst.x +"coord.x =" + coord.x)
+      if (coordTst.x == coord.x && coordTst.y == coord.y) {
+        macthed = true;
+
+        {
+    {
+    if(!matched) {
+      map[coordTst.y][coordTst.x]= food;
+    return map;
+      }
+    }
+  }
+}
+function placeSnake(map){
+  var width = map[0].length;
+  var height = map.length;
+  var snake =[]
+  snake[0] = ranCoord(width, length);
+  snake[1] = nieghbor(snake[0], width, length);
+  return snake;
+}
+
+
+
+function ranCoord(xVal, yVal){
+  var retObj = {};
+  retObj.x = Math.floor(math.random() * xVal));
+  retObj.x = Math.floor(math.random() * yVal));
+}
+
+function neighborCoord(coord, width, height) {
+  var xUse = false;
+  if (Math.random() < .5 ) { xUse == true; }
+  if (xUse){
+    var left = false;
+    if (Math.random() < .5 && coord.x > 0) {up = true; }
+  }
+
+  }
+}
--- snekrules.js
+++ snekrules.js
@@ -1,3 +1,7 @@
-//the area is 30 by 30
+
 const length = 30;
 const width = 30;
+
+var map = makeMap(width, length);
+document.getElementById("map").innerHTML = borderMap(map);
+document.getElementById("score").innerHTML = "score =" + score;

006fce0a0db4baeafece727123ee372a704efeae
Bradley Hume <Bhume16@gmail.com>
2019-11-12 09:28:43 -0500

    Made snake

--- javascript.js
+++ javascript.js
@@ -5,6 +5,30 @@ const width = 30;
 var snake = [];
 //Test code
 var map = makeMap(width, length);
+snake = placeSnake(map)
+map = seeSnake(map,snake)
 var score = snake.length -2;
 document.getElementById("map").innerHTML = borderMap(map);
 document.getElementById("score").innerHTML = "score =" + score;
+
+
+document.addEventListener('keydown',function(event) {
+  if(event.key =='w' ){
+    document.getElementById("wKey").style.backgroundColor = "#ffdb58";}
+  if(event.key =='s'){
+    document.getElementById("sKey").style.backgroundColor = "#ffdb58";}
+  if(event.key =='a'){
+    document.getElementById("aKey").style.backgroundColor = "#ffdb58";}
+    if(event.key =='d'){
+    document.getElementById("dKey").style.backgroundColor = "#ffdb58";}
+});
+document.addEventListener('keyup',function(event) {
+  if(event.key =='w' ){
+    document.getElementById("wKey").style.backgroundColor = "#ffdb58";}
+  if(event.key =='s'){
+    document.getElementById("sKey").style.backgroundColor = "#ffdb58";}
+  if(event.key =='a'){
+    document.getElementById("aKey").style.backgroundColor = "#ffdb58";}
+    if(event.key =='d'){
+    document.getElementById("dKey").style.backgroundColor = "#ffdb58";}
+});

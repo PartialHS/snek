657b64b6e42dce35864348120525ba4c23e69711
Bradley Hume <Bhume16@gmail.com>
2019-11-14 09:11:04 -0500

    pull Mike

--- game.js
+++ game.js
@@ -74,26 +74,7 @@ function borderMap(text){
   return retStr;
 }
 
-function placeFood(map, snake, food="*"){
-  var width = map[0].length;
-  var height = map.length;
-  var place = false;
 
-  while (!place) {
-    var coordTst = ranCoord(width, height);
-    var matched = false;
-    for (obj in snake){
-
-        if(coordTst.x == snake[obj].x && coordTst.y == snake[obj].y){
-        matched = true;
-      }
-    }
-    if (!matched){
-      map[coordTst.y][coordTst.x] = food;
-      return map;
-    }
-  }
-}
 
 function ranCoord(xVal, yVal){
   var retObj = {};
@@ -162,6 +143,17 @@ does the remove before the addition
 @param food {bool} if there is a food
 @return the new location for the snake
 */
+
+/* moveSnake(snake, dir, food)
+takes a snake array and adds a new item to the front of it in direction dir. (n, s, e w)
+removes last item from the snake array as well, if food is equal to false
+does the remove before the addition
+@prama snake {array} an array represeting a snake
+@prama dir {string} the string "w", "a", "s", or "d"
+@param food {object} the location of the food
+@return {array}the new location for the snake
+*/
+
 function moveSnake(snake, dir, food=false){
 
 if (dir == "w"){
@@ -175,9 +167,104 @@ if (dir == "a"){
 }
 if (dir == "d"){
       snake.unshift({x:snake[0].x +1, y: snake[0].y})
-}
-if (!food){
-  snake.pop();
+} // snake[0] == food
+if (!(snake[0].x == food.x && snake[0].y == food.y)){
+   snake.pop();
  }
  return snake;
+}
+
+/* makeFood(map, snake)
+uses brute force to try to place food on the map. Rolls a random location, then
+checks to see if it is in snake
+@param map {array} an array of arrays
+@param snake {array} an array of the cords of the snake
+@return {object} an object with x and y coordinates that are good for
+food placement and a food key with the icon for the food.
+*/
+
+function makeFood(map, snake, food="*"){
+  var width = map[0].length;
+  var height = map.length;
+  var place = false;
+
+  while (!place) {
+    var coordTst = ranCoord(width, height);
+    var matched = false;
+    for (obj in snake){
+
+        if(coordTst.x == snake[obj].x && coordTst.y == snake[obj].y){
+        matched = true;
+      }
+    }
+    if (!matched){
+      coordTst.food = food;
+      return coordTst;
+    }
+  }
+}
+
+/* seeFood(map, food)
+makes the food visable on the map
+@param map {array} an 2d array of strings of text
+@param food {object} a food object with x, y, and food keys
+@return {array}a modified map
+*/
+function seeFood(map, food){
+  map[food.y][food.x] = food.food;
+return map;
+}
+
+/* updateMap(snake, key, food, width, length)
+a wrapper function have all assignments work within a single function
+@param snake {array} an array of x and y keyed objects
+@param key {string} should be a single character that is w,s,a, or d
+@param food {object} a food object with x, y, and food keys
+@param width {int} max x value for map
+@param length {int} max y value for map
+@return {array} the updated version of the map
+*/
+function updateMap(snake, key, food, width, length){
+  snake = moveSnake(snake, key, food);
+  map = makeMap(width, length);
+  map = seeSnake(map, snake);
+  food = checkAte(map, snake, food);
+  map = seeFood(map, food);
+  document.getElementById("map").innerHTML = borderMap(map);
+  return food;
+}
+/* checkAte(map, snake, food)
+A function to see if the current food location is the location of the snake head
+if so, makes a new location for the food
+@param map {array} a 2d array of elements that are single string characters
+@param snake {array} an array of x & y keyed objects
+@param food {object} an object with x, y, & food keys that is the location of
+the food.
+@return {object} the new or same location of the food.
+*/
+function checkAte(map, snake, food){
+if (snake[0].x == food.x && snake[0].y == food.y) {
+  return makeFood(map, snake, food.food);
+  }
+  return food;
+}
+
+/*function checkCollision(snake, width, length)
+@param snake {array} an array of x and y keyed objects
+@param width {int} max x value for map
+@param length {int} max y value for map
+@return {bool} true if collision
+*/
+
+function checkCollision(snake, width, length){
+  var hit = false;
+  if (snake[0].x <= width && snake[0].y <= length) {
+    return hit = true;
+  }
+  for (var i = 0; i < snake.length; i++) {
+    if (snake[0].x == snake.length.x && snake[0].y == snake.length.y) {
+      return true;
+    }
+
+  }
 }
--- javascript.js
+++ javascript.js
@@ -3,10 +3,13 @@ const length = 30;
 const width = 30;
 
 var snake = [];
+var food = null;
 //Test code
 var map = makeMap(width, length);
 snake = placeSnake(map);
 map = seeSnake(map, snake);
+food = makeFood(map, snake);
+map = seeFood(map, food);
 var score = snake.length -2;
 document.getElementById("map").innerHTML = borderMap(map);
 document.getElementById("score").innerHTML = "score =" + score;
@@ -26,38 +29,27 @@ document.addEventListener('keyup',function(event) {
 
   if(event.key =='w' ){
     document.getElementById("wKey").style.backgroundColor = "#ffdb58";
-  snake = moveSnake(snake, "w");
-  map = makeMap(width, length);
-  map = seeSnake(map, snake);
+  food = updateMap(snake, "w", food, width, length)
   var score = snake.length -2;
-  document.getElementById("map").innerHTML = borderMap(map);
   document.getElementById("score").innerHTML = "score =" + score;
 }
   if(event.key =='s'){
     document.getElementById("sKey").style.backgroundColor = "#ffdb58";
-    snake = moveSnake(snake, "s");
-    map = makeMap(width, length);
-    map = seeSnake(map, snake);
+    food =updateMap(snake, "s", food, width, length);
     var score = snake.length -2;
-    document.getElementById("map").innerHTML = borderMap(map);
     document.getElementById("score").innerHTML = "score =" + score;
   }
   if(event.key =='a'){
     document.getElementById("aKey").style.backgroundColor = "#ffdb58";
-    snake = moveSnake(snake, "a");
-    map = makeMap(width, length);
-    map = seeSnake(map, snake);
+    food = updateMap(snake, "a", food, width, length);
     var score = snake.length -2;
-    document.getElementById("map").innerHTML = borderMap(map);
     document.getElementById("score").innerHTML = "score =" + score;
   }
     if(event.key =='d'){
     document.getElementById("dKey").style.backgroundColor = "#ffdb58";
-    snake = moveSnake(snake, "d");
-    map = makeMap(width, length);
-    map = seeSnake(map, snake);
+    food = updateMap(snake, "d", food, width, length);
     var score = snake.length -2;
-    document.getElementById("map").innerHTML = borderMap(map);
+
     document.getElementById("score").innerHTML = "score =" + score;
   }
 });
